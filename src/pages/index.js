f4f530c0fb4f8b5be5d4d6044a8f422366e4a7ec
Carlos Filoteo <c.filoteo@live.com>
2018-05-28 23:31:56 -0600

    Add more es7/8 content

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,6 +75,64 @@ console.log(z); // { a: 3, b: 4 }`}</code>
 '42'.padEnd(8, '0')   // '42000000'`}</code>
               </pre>
             </section>
+            <section id="object-entries" className="main">
+              <h2>Object.entries</h2>
+              <p>
+                <code>Object.entries</code> returns an array of a given object's
+                own enumerable properties as <code>[key, value]</code> pairs, in
+                the same order as that provided by a <code>for...in</code> loop.
+              </p>
+              <pre>
+                <code
+                >{`Object.entries({ 'a': 'A', 'b': 'B' }); // [["a","A"],["b","B"]]`}</code>
+              </pre>
+            </section>
+            <section id="object-values" className="main">
+              <h2>Object.values</h2>
+              <p>
+                <code>Object.values</code> returns an array of a given object's
+                own enumerable property values, in the same order as that
+                provided by a <code>for...in</code> loop.
+              </p>
+              <pre>
+                <code>{`// Object literal
+Object.values({ 'a': 23, 'b': 19 }) // [23, 19]
+
+// Array-like object (order not preserved)
+Object.values({ 80: 'eighty', 0: 1, 1: 'yes' }) // [1, 'yes', 'eighty']`}</code>
+              </pre>
+            </section>
+            <section id="array-prototype-includes" className="main">
+              <h2>Array.prototype.includes</h2>
+              <p>
+                <code>includes()</code> determines whether an array includes a
+                certain element, returning true or false as appropriate.
+              </p>
+              <pre>
+                <code
+                >{`['a', 'b', 'c'].includes('a') // true, not 0 like indexOf would give
+['a', 'b', 'c'].includes('d') // false`}</code>
+              </pre>
+            </section>
+            <section id="exponentiation" className="main">
+              <h2>Exponentiation</h2>
+              <p>
+                The exponentiation operator is a shorthand method for the{' '}
+                <code>Math.pow</code> function, allowing for a more succinctly
+                infix notation.
+              </p>
+              <pre>
+                <code>{`// 2 to the power of 8
+Math.pow(2, 8) // 256
+
+// ..becomes
+2 ** 8 // 256
+
+let value = 5;
+value **= 2;
+// value: 25`}</code>
+              </pre>
+            </section>
           </main>
         </div>
       </div>
